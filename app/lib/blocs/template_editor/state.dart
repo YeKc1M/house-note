8e736e41ef67bd7700bd118e6bc3776630bf068b
YeKc1M <15008181891@163.com>
2026-04-15 20:54:14 +0800

    feat(template_editor): add thumbnailDimensionIds to state

--- app/lib/blocs/template_editor/state.dart
+++ app/lib/blocs/template_editor/state.dart
@@ -4,22 +4,26 @@ import '../../models/dimension_node.dart';
 class TemplateEditorState extends Equatable {
   final String templateName;
   final List<DimensionNode> dimensions;
+  final List<String> thumbnailDimensionIds;
 
   const TemplateEditorState({
     this.templateName = '',
     this.dimensions = const [],
+    this.thumbnailDimensionIds = const [],
   });
 
   TemplateEditorState copyWith({
     String? templateName,
     List<DimensionNode>? dimensions,
+    List<String>? thumbnailDimensionIds,
   }) {
     return TemplateEditorState(
       templateName: templateName ?? this.templateName,
       dimensions: dimensions ?? this.dimensions,
+      thumbnailDimensionIds: thumbnailDimensionIds ?? this.thumbnailDimensionIds,
     );
   }
 
   @override
-  List<Object?> get props => [templateName, dimensions];
+  List<Object?> get props => [templateName, dimensions, thumbnailDimensionIds];
 }
